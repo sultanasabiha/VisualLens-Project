2f33938e83eb1fbc2afc221a1e597c7a0eb9eb16
Sabiha Sultana <87910631+sultanasabiha@users.noreply.github.com>
2024-03-28 14:24:55 +0530

    Update test.py
    Introduced oops in the gui, introduced changable frames

--- test.py
+++ test.py
@@ -1,131 +1,155 @@
-from tkinter import *
+import tkinter as tk
+
 from tkinter import filedialog
-root=Tk()
-root.title("Test Version")
-root.geometry("400x500")
+
+class checkOptions(tk.Frame):
+    def __init__(self, parent=None, picks=[], anchor=tk.W):
+        tk.Frame.__init__(self, parent)
+        self.vars = []
+        for pick in picks:
+            var = tk.IntVar()
+            chk = tk.Checkbutton(self, text=pick, variable=var)
+            chk.pack(side="top",expand=True,anchor="w")
+            self.vars.append(var)
+    def state(self):
+        return map((lambda var: var.get()), self.vars)
+    
+
+
+class Classification(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        tk.Label(self,text="Machine Learning Algorithms :").pack(side="top",anchor="w")
+        self.mloptions = checkOptions(self,["Naive Bayes Model","Decision Tree Model","Random Forest Model","Bagging Model"])
+        self.mloptions.pack(side=tk.TOP,  fill=tk.X,anchor="w")
+        self.mloptions.config(relief=tk.GROOVE, bd=2,padx=10,pady=10)
+
+        tk.Label(self,text="Transfer Learning Algorithms :").pack(side="top",anchor="w",pady=(10,0))
+        self.tloptions = checkOptions(self, ["MobileNet Model","ResNet50","VGG16","VGG19"])
+        self.tloptions.pack(side="top",fill="x",anchor="w")
+        self.tloptions.config(relief=tk.GROOVE, bd=2,padx=10,pady=10)
+
+        tk.Button(self, text='Back',command=lambda: controller.show_frame(Start)).pack(side="bottom")
+        tk.Button(self, text='Peek', command=self.allstates).pack(side="bottom")
+
+
+    def allstates(self): 
+        print(list(self.mloptions.state()), list(self.tloptions.state()))
+class Clustering(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        tk.Label(self,text="Machine Learning Algorithms :").pack(side="top",anchor="w")
+        self.mloptions = checkOptions(self, ["K Means Algorithm"])
+        self.mloptions.pack(side=tk.TOP,  fill=tk.X,anchor="w")
+        self.mloptions.config(relief=tk.GROOVE, bd=2,padx=10,pady=10)
+
+        tk.Label(self,text="Transfer Learning Algorithms :").pack(side="top",anchor="w",pady=(10,0))
+        self.tloptions = checkOptions(self, ["MobileNet Model","ResNet50","VGG16","VGG19"])
+        self.tloptions.pack(side="top",fill="x",anchor="w")
+        self.tloptions.config(relief=tk.GROOVE, bd=2,padx=10,pady=10)
+
+        tk.Button(self, text='Back',command=lambda: controller.show_frame(Start)).pack(side="bottom")
+        tk.Button(self, text='Peek', command=self.allstates).pack(side="bottom")
+
+
+    def allstates(self): 
+        print(list(self.mloptions.state()), list(self.tloptions.state()))
+
+  
+class Container(tk.Tk):
+    def __init__(self,*args,**kwargs):
+        tk.Tk.__init__(self,*args,**kwargs)
+        root=tk.Frame(self)
+        root.pack(side="top", fill="both", expand = True,padx=10,pady=10,ipady=5,ipadx=5)
+        root.grid_columnconfigure(0, weight=4)
+        root.grid_columnconfigure(1, weight=1)
+
+        self.frames={}
+        for F in (Start,Similarity,Classification,Clustering):
+            frame=F(root,self)
+            self.frames[F]=frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        self.show_frame(Start)
+    def show_frame(self,con):
+        frame=self.frames[con]
+        frame.tkraise()
+
+class Start(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        f=tk.Frame(self)
+        f.pack(side="top",expand=True)
+        self.flag=False
+        tk.Label(f,text="Upload an image dataset :").pack(side="left",anchor="w")
+        tk.Button(f,text="Browse ",command=lambda:self.openfile(controller)).pack(side="right",anchor="e")
+
+        f=tk.LabelFrame(self,text="Select an application --",pady=10,padx=10)
+        f.pack(side="top",fill="both",expand=True,anchor="w",padx=10,pady=10)
+
+        Apps=[
+            ("Image Similarity",1),
+            ("Image Classification",2),
+            ("Image Clustering",3)
+        ]
+        self.choice=tk.IntVar()
+        self.choice.set(0)
+        for app,value in Apps:
+            tk.Radiobutton(f,text=app,variable=self.choice,value=value).pack(side="top",anchor="w")
+        self.btn=tk.Button(self,text="Proceed",command=lambda:self.getchoice(controller))
+        self.btn.pack(side="top",fill="x",expand=True)
+        self.t=tk.Label(self)
+        self.t.pack(side="top",fill="x",expand=True)
+
+    def getchoice(self,controller):
+        if self.flag==False:
+            self.t.config(text="Dataset not uploaded")
+            self.flag=False
+        else:
+            if self.choice.get()==0:
+                self.t.config(text="Please select a task")
+            elif(self.choice.get()==1):
+                self.t.config(text="")
+                go=Similarity
+            elif(self.choice.get()==2):
+                self.t.config(text="")
+                go=Classification
+            elif(self.choice.get()==3):
+                self.t.config(text="")
+                go=Clustering
+            controller.show_frame(go)
+
+  
+    def openfile(self,controller):
+        self.filename=filedialog.askopenfilename(initialdir="c:\\Users\\Sabiha\\Downloads",title="Select a file", filetypes=(("All Files", "*.*"),("CSV Files", "*.csv")))
+        self.t.config(text="File is successfully loaded")
+        self.flag=True
+
+
+class Similarity(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        tk.Label(self,text="Machine Learning Algorithms :").pack(side="top",anchor="w")
+        self.mloptions = checkOptions(self, ["Cosine Similarity","Euclidean Distance","Manhattan Distance"])
+        self.mloptions.pack(side=tk.TOP,  fill=tk.X,anchor="w")
+        self.mloptions.config(relief=tk.GROOVE, bd=2,padx=10,pady=10)
+
+        tk.Label(self,text="Transfer Learning Algorithms :").pack(side="top",anchor="w",pady=(10,0))
+        self.tloptions = checkOptions(self, ["MobileNet Model","ResNet50","VGG16","VGG19"])
+        self.tloptions.pack(side="top",fill="x",anchor="w")
+        self.tloptions.config(relief=tk.GROOVE, bd=2,padx=10,pady=10)
+
+        tk.Button(self, text='Back',command=lambda: controller.show_frame(Start)).pack(side="bottom")
+        tk.Button(self, text='Peek', command=self.allstates).pack(side="bottom")
+
+
+    def allstates(self): 
+        print(list(self.mloptions.state()), list(self.tloptions.state()))
 
     
-def proceed(val):
 
-    if(val==1):
-        frame1=LabelFrame(root,padx=10,pady=10,height=250,width=250)
-        frame1.grid(row=8,column=0,sticky=W+E,padx=10,pady=10)
-        frame1.grid_propagate(0)
-        Label(frame1,text="Machine Learning Algorithms :").grid(row=9,column=0)
-        algos_trad=[
-            ("Cosine Similarity",1),
-            ("Euclidean Distance",2),
-            ("Manhattan Distance",3)
-        ]
-        algos=IntVar()
-        algos.set(1)
-        row=10
-        for algo,value in algos_trad:
-            Radiobutton(frame1,text=algo,variable=algos,value=value).grid(row=row,column=0,sticky=W)
-            row+=1
-        trad=algos.get()
-        
-        Label(frame1,text="Transfer Learning Algorithms :").grid(row=13,column=0)
-        algos_trans=[
-            ("MobileNet Model",4),
-            ("ResNet50",5),
-            ("VGG16",6),
-            ("VGG19",7)
-        ]
-        algost=IntVar()
-        algost.set(1)
-        row=14
-        for algo,value in algos_trans:
-            Radiobutton(frame1,text=algo,variable=algost,value=value).grid(row=row,column=0,sticky=W)
-            row+=1
-        trans=algost.get()
-        
-
-    if(val==2):
-
-        frame2=LabelFrame(root,padx=10,pady=10,height=250,width=250)
-        frame2.grid(row=8,column=0,sticky=W+E,padx=10,pady=10)
-        frame2.grid_propagate(0)
-        Label(frame2,text="Machine Learning Algorithms :").grid(row=9,column=0)
-        algos_trad=[
-            ("Naive Bayes Model",1),
-            ("Decision Tree Model",2),
-            ("Random Forest Model",3),
-            ("Bagging Model",4)
-        ]
-        algos=IntVar()
-        algos.set(1)
-        row=10
-        for algo,value in algos_trad:
-            Radiobutton(frame2,text=algo,variable=algos,value=value).grid(row=row,column=0,sticky=W)
-            row+=1
-        trad=algos.get()
-        
-        Label(frame2,text="Transfer Learning Algorithms :").grid(row=14,column=0)
-        algos_trans=[
-        ("MobileNet Model",4),
-        ("ResNet50",5),
-        ("VGG16",6),
-        ("VGG19",7)
-        ]
-        algost=IntVar()
-        algost.set(1)
-        row=15
-        for algo,value in algos_trans:
-            Radiobutton(frame2,text=algo,variable=algost,value=value).grid(row=row,column=0,sticky=W)
-            row+=1
-        trans=algost.get()
-    if(val==3):
-
-        frame3=LabelFrame(root,padx=10,pady=10,height=250,width=250)
-        frame3.grid(row=8,column=0,sticky=W+E,padx=10,pady=10)
-        frame3.grid_propagate(0)
-        Label(frame3,text="Machine Learning Algorithms :").grid(row=9,column=0,sticky=W)
-        algos_trad=[
-            ("K Means Algorithm",1)
-        ]
-        algos=IntVar()
-        algos.set(1)
-        row=10
-        for algo,value in algos_trad:
-            Radiobutton(frame3,text=algo,variable=algos,value=value).grid(row=row,column=0,sticky=W)
-            row+=1
-        trad=algos.get()
-        
-        Label(frame3,text="Transfer Learning Algorithms :").grid(row=11,column=0)
-        algos_trans=[
-        ("MobileNet Model",4),
-        ("ResNet50",5),
-        ("VGG16",6),
-        ("VGG19",7)
-        ]
-        algost=IntVar()
-        algost.set(1)
-        row=12
-        for algo,value in algos_trans:
-            Radiobutton(frame3,text=algo,variable=algost,value=value).grid(row=row,column=0,sticky=W)
-            row+=1
-        trans=algost.get()
-def open():
-    root.filename=filedialog.askopenfilename(initialdir="c:\\Users\\Sabiha\\Downloads",title="Select a file", filetypes=(("All Files", "*.*"),("CSV Files", "*.csv")))
-    Label(root,text="File is successfully loaded").grid(row=1,column=0,sticky=W)
-    
-    Label(root,text="Select an application --",pady=10).grid(row=2,column=0,sticky=W)
-    Apps=[
-    ("Image Similarity",1),
-    ("Supervised Image Classification",2),
-    ("Unsupervised Image Clustering",3)
-    ]
-    choice=IntVar()
-    choice.set(0)
-    row=3
-    for app,value in Apps:
-        Radiobutton(root,text=app,variable=choice,value=value).grid(row=row,column=0,sticky=W)
-        row+=1
-    btn=Button(root,text="Proceed",command=lambda: proceed(choice.get())).grid(row=7,column=0)
- 
-open_label=Label(root,text="Upload an image dataset :").grid(row=0,column=0,sticky=W)
-open_button=Button(root,text="Browse ",command=open).grid(row=0,column=1,sticky=W)
+app=Container()
+app.mainloop()
 
 
-root.mainloop()
+
+   
