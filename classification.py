feb5c5eaa5b18d6c53e289a8199b5c79ea1506ea
Sabiha Sultana <87910631+sultanasabiha@users.noreply.github.com>
2024-04-18 19:40:11 +0530

    Update classification.py
    generalized input intake

--- classification.py
+++ classification.py
@@ -1,154 +1,202 @@
 #Importing necessary libraries for reading images
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
-from skimage.io import imread,imshow
-from glob import glob
-from skimage.transform import resize
-#Importing necessary libraries for using Transfer Learning algorithm
+import cv2
+import os
 from keras.preprocessing import image as kimage
-from keras.applications.mobilenet import preprocess_input
-from keras.applications import MobileNet
-from keras.applications.resnet50 import preprocess_input
-from keras.applications import ResNet50
-from keras.applications.vgg16 import preprocess_input
-from keras.applications import VGG16
-from keras.applications.vgg19 import preprocess_input
-from keras.applications import VGG19
-#Importing necessary libraries for metrics evaluation
+
 np.random.seed(1000)
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 
 class Classify:
-    def __init__(self):
-        #Creating list of categories for representing dependent variables
-        self.y=[1,2,3,2,2,2,2,1,3,4,1,2,4,2,3,3,4,2,2,4,2,2,3,5,5,5,1,4,2,2,2,3,3,1,1,3,1,1,4,2,5,5,5,1,5,2,4,5,1,3,5,3,4,3,1,4,5,3,1,6,3,5,5,4,3,6,4,4,6,4,3,4,6,4,6,6,6,6,6,6,6]
-        #Creating list of categories
-        self.category=["Badger","Antelope","Bear","Bee","Butterfly","Bat"]
+    def __init__(self,category_file,image_dir,test):
+        # Path to the text file containing class names
+        self.category_file=category_file
+        # Directory where images are located
+        self.images_dir=image_dir
+        # Directory where test image is located
+        self.test=test
+        
+        # Read class names from the text file
+        with open(self.category_file, 'r') as file:
+            self.categories = [line.strip() for line in file]
+
+        
+        # Map class names to their respective image folders
+        self.category_to_folder = {category: os.path.join(image_dir, category) for category in self.categories}
+
+        self.labels=[]
         self.x_trg,self.x_test,self.y_trg,self.y_test=0,0,0,0
         self.name=""
     def setdataset(self,matrix):
         #Creating training test datasets for dependent and independent variables
-        self.x_trg,self.x_test,self.y_trg,self.y_test=train_test_split(matrix,self.y,random_state=0,test_size=0.2)
+        self.x_trg,self.x_test,self.y_trg,self.y_test=train_test_split(matrix,self.labels,random_state=0,test_size=0.2)
         print("Dimension of training, test dataset: ",self.x_trg.shape,self.x_test.shape)
 
-    def readimage(self,train,test):
-        i=1
-        matrix=np.zeros([81,750000]) 
+    def readimage(self):
 
-        #Reading and resizing the images
-        for image in glob(train+'/*.jpg'):
-            image_form=imread(image,as_gray=False)
+        images = []
+        labels = []
+        for category, folder_path in self.category_to_folder.items():
+            for filename in os.listdir(folder_path):
+                img_path = os.path.join(folder_path, filename)
+                try:
+                    img_form = cv2.imread(img_path)
+                    img_resized = cv2.resize(img_form, (500, 500))
+                    #img = img.astype(np.float32) / 255.0  # Normalize pixel values
+                    images.append(img_resized.flatten())
+                    labels.append(self.categories.index(category))
+                except Exception as e:
+                    print(f"Error loading image '{img_path}': {e}")
+        self.labels=np.array(labels)
 
-            #Resizing all the images to same dimension
-            img_resized=resize(image_form,(500,500))
-
-            #Flattening the images to one dimension for applying algorithms
-            item=img_resized.flatten()
-            matrix[i-1,:]=item
-            i=i+1       
-
-        #Reading the test image for prediction
-        image = imread(test,as_gray=False)
-        img_resized=resize(image,(500,500))
         item=np.zeros([1,750000])
+        img= cv2.imread(self.test)
+        img_resized= cv2.resize(img, (500, 500))
         item[0,:]=img_resized.flatten()
 
-        self.setdataset(matrix)
+        self.setdataset(np.array(images))
         return item
 
-    def readimageTL(self,train):
-        images_dict=dict()
+    def mobnet(self):
+        from keras.applications.mobilenet import preprocess_input
+        from keras.applications import MobileNet
+        images = []
+        labels = []
+        for category, folder_path in self.category_to_folder.items():
+            for filename in os.listdir(folder_path):
+                img_path = os.path.join(folder_path, filename)
+                try:
+                    img = kimage.load_img(img_path, target_size=(224, 224))
+                    img = preprocess_input(np.expand_dims(kimage.img_to_array(img),axis=0))
+                    images.append(img)
+                    labels.append(self.categories.index(category))
+                except Exception as e:
+                    print(f"Error loading image '{img_path}': {e}")
+        self.labels=np.array(labels)
+        images= np.array(images)
+        # Load MobileNet model
+        mobile_net_model = MobileNet(weights='imagenet', include_top=False)
 
-        #Processing all images according to mobilenet algorithm
-        for mix_image in glob(train+'/*.jpg'):
+        # Predict features for each preprocessed image
+        features_list = []
+        for preprocessed_image in images:
+            # Predict features for the preprocessed image
+            features = mobile_net_model.predict(preprocessed_image)
+            features_list.append(features.ravel())
 
-            #Generally accepted image size for trained models is 224 X 224 px
-            image=kimage.load_img(mix_image,target_size=(224,224))
-
-            #Converting image to array form
-            image=preprocess_input(np.expand_dims(kimage.img_to_array(image),axis=0))
-
-            #Extracting image name from the path
-            num=mix_image.split('\\')[-1].split('.')[0]
-
-            #Mapping image and id
-            images_dict[num]=image
-        return images_dict
-    def mobnet(self,images_dict,test):
-        #Creating the model
-        mobile_net_model=MobileNet(include_top=False,weights='imagenet')
-
-        #Creating the matrix and initializing matrix to 0
-        images_matrix2=np.zeros([81,50176])
-        for i, (num,image) in enumerate(images_dict.items()):
-
-            #Flatten the matrix after using mobilenet model
-            images_matrix2[i,:]=mobile_net_model.predict(image).ravel()
-
-        image=kimage.load_img(test,target_size=(224,224))
+        image=kimage.load_img(self.test,target_size=(224,224))
         image=preprocess_input(np.expand_dims(kimage.img_to_array(image),axis=0))
         item=np.zeros([1,50176])
         item[0,:]=mobile_net_model.predict(image).ravel()
-        self.setdataset(images_matrix2)
+        self.setdataset(np.array(features_list))
         self.name=" with MobileNet"
         return item
 
-    def resnet(self,images_dict,test):
+    def resnet(self):
+        from keras.applications.resnet50 import preprocess_input
+        from keras.applications import ResNet50
+
+        images = []
+        labels = []
+        for category, folder_path in self.category_to_folder.items():
+            for filename in os.listdir(folder_path):
+                img_path = os.path.join(folder_path, filename)
+                try:
+                    img = kimage.load_img(img_path, target_size=(224, 224))
+                    img = preprocess_input(np.expand_dims(kimage.img_to_array(img),axis=0))
+                    images.append(img)
+                    labels.append(self.categories.index(category))
+                except Exception as e:
+                    print(f"Error loading image '{img_path}': {e}")
+        self.labels=np.array(labels)
+        images= np.array(images)
         #Creating the model
         res_net_model=ResNet50(include_top=False,weights='imagenet')
 
-        #Creating the matrix and initializing matrix to 0
-        images_matrix=np.zeros([81,100352])
-        for i, (num,image) in enumerate(images_dict.items()):
-
-            #Flatten the matrix after using mobilenet model
-            images_matrix[i,:]=res_net_model.predict(image).ravel()
-
-        image=kimage.load_img(test,target_size=(224,224))
+        # Predict features for each preprocessed image
+        features_list = []
+        for preprocessed_image in images:
+            # Predict features for the preprocessed image
+            features =res_net_model.predict(preprocessed_image)
+            features_list.append(features.ravel())
+        
+        image=kimage.load_img(self.test,target_size=(224,224))
         image=preprocess_input(np.expand_dims(kimage.img_to_array(image),axis=0))
         item=np.zeros([1,100352])
         item[0,:]=res_net_model.predict(image).ravel()
-        self.setdataset(images_matrix)
+        self.setdataset(np.array(features_list))
         self.name=" with ResNet"
         return item
 
-    def vgg16(self,images_dict,test):
+    def vgg16(self):
+        from keras.applications.vgg16 import preprocess_input
+        from keras.applications import VGG16
+
+        images = []
+        labels = []
+        for category, folder_path in self.category_to_folder.items():
+            for filename in os.listdir(folder_path):
+                img_path = os.path.join(folder_path, filename)
+                try:
+                    img = kimage.load_img(img_path, target_size=(224, 224))
+                    img = preprocess_input(np.expand_dims(kimage.img_to_array(img),axis=0))
+                    images.append(img)
+                    labels.append(self.categories.index(category))
+                except Exception as e:
+                    print(f"Error loading image '{img_path}': {e}")
+        self.labels=np.array(labels)
+        images= np.array(images)
         #Creating the model
         vgg16_model=VGG16(include_top=False,weights='imagenet')
 
-        #Creating the matrix and initializing matrix to 0
-        images_matrix=np.zeros([81,25088])
-        for i, (num,image) in enumerate(images_dict.items()):
+        # Predict features for each preprocessed image
+        features_list = []
+        for preprocessed_image in images:
+            # Predict features for the preprocessed image
+            features = vgg16_model.predict(preprocessed_image)
+            features_list.append(features.ravel())
+            image=kimage.load_img(self.test,target_size=(224,224))
 
-            #Flatten the matrix after using mobilenet model
-            images_matrix[i,:]=vgg16_model.predict(image).ravel()
-
-        image=kimage.load_img(test,target_size=(224,224))
         image=preprocess_input(np.expand_dims(kimage.img_to_array(image),axis=0))
         item=np.zeros([1,25088])
         item[0,:]=vgg16_model.predict(image).ravel()
-        self.setdataset(images_matrix)
+        self.setdataset(np.array(features_list))
         self.name=" with VGG16"
         return item
 
-    def vgg19(self,images_dict,test):
+    def vgg19(self):
+        from keras.applications.vgg19 import preprocess_input
+        from keras.applications import VGG19
+
+        images = []
+        labels = []
+        for category, folder_path in self.category_to_folder.items():
+            for filename in os.listdir(folder_path):
+                img_path = os.path.join(folder_path, filename)
+                try:
+                    img = kimage.load_img(img_path, target_size=(224, 224))
+                    img = preprocess_input(np.expand_dims(kimage.img_to_array(img),axis=0))
+                    images.append(img)
+                    labels.append(self.categories.index(category))
+                except Exception as e:
+                    print(f"Error loading image '{img_path}': {e}")
+        self.labels=np.array(labels)
+        images= np.array(images)
         #Creating the model
         vgg19_model=VGG19(include_top=False,weights='imagenet')
 
-        #Creating the matrix and initializing matrix to 0
-        images_matrix=np.zeros([81,25088])
-        for i, (num,image) in enumerate(images_dict.items()):
-
-            #Flatten the matrix after using mobilenet model
-            images_matrix[i,:]=vgg19_model.predict(image).ravel()
-
-        image=kimage.load_img(test,target_size=(224,224))
+        # Predict features for each preprocessed image
+        features_list = []
+        for preprocessed_image in images:
+            # Predict features for the preprocessed image
+            features = vgg19_model.predict(preprocessed_image)
+            features_list.append(features.ravel())
+        
+        image=kimage.load_img(self.test,target_size=(224,224))
         image=preprocess_input(np.expand_dims(kimage.img_to_array(image),axis=0))
         item=np.zeros([1,25088])
         item[0,:]=vgg19_model.predict(image).ravel()
-        self.setdataset(images_matrix)
+        self.setdataset(np.array(features_list))
         self.name=" with VGG19"
         return item
 
@@ -164,14 +212,13 @@ class Classify:
 
         #Determining accuracy and creating confusion matrix of the model
         naive_pred=naive_model.predict(self.x_test)
-        naive_acc_score=accuracy_score(self.y_test,naive_pred)
         y_probas = naive_model.predict_proba(self.x_test)
 
         #Determining accuracy of the test image
         naive_pred_item=naive_model.predict(item)
         naive_value=naive_pred_item[0]
-
-        return naive_model,naive_pred,y_probas,naive_acc_score,title,self.category[naive_value-1],self.x_trg,self.y_trg,self.y_test
+        print(naive_pred_item,self.categories)
+        return naive_model,naive_pred,y_probas,title,self.categories[naive_value],self.x_trg,self.y_trg,self.y_test
 
     def decision(self,item):
 
@@ -184,13 +231,12 @@ class Classify:
 
         #Determining accuracy and creating confusion matrix of the model
         tree_pred=tree_model.predict(self.x_test)
-        tree_acc_score=accuracy_score(self.y_test,tree_pred)
         y_probas = tree_model.predict_proba(self.x_test)
 
         #Determining accuracy of the test image
         tree_pred_item=tree_model.predict(item)
         tree_value=tree_pred_item[0]
-        return tree_model,tree_pred,y_probas,tree_acc_score,title,self.category[tree_value-1],self.x_trg,self.y_trg,self.y_test
+        return tree_model,tree_pred,y_probas,title,self.categories[tree_value],self.x_trg,self.y_trg,self.y_test
 
     def forest(self,item):
         title="Random Forest"+self.name
@@ -202,13 +248,12 @@ class Classify:
 
         #Determining accuracy and creating confusion matrix of the model
         forest_pred=forest_model.predict(self.x_test)
-        forest_acc_score=accuracy_score(self.y_test,forest_pred)
         y_probas = forest_model.predict_proba(self.x_test)
 
         #Determining accuracy of the test image
         forest_pred_item=forest_model.predict(item)
         forest_value=forest_pred_item[0]
-        return forest_model,forest_pred,y_probas,forest_acc_score,title,self.category[forest_value-1],self.x_trg,self.y_trg,self.y_test
+        return forest_model,forest_pred,y_probas,title,self.categories[forest_value],self.x_trg,self.y_trg,self.y_test
 
     def bag(self,item):
         title="Bagging Model"+self.name
@@ -220,11 +265,10 @@ class Classify:
 
         #Determining accuracy and creating confusion matrix of the model
         bag_pred=bag_model.predict(self.x_test)
-        bag_acc_score=accuracy_score(self.y_test,bag_pred)
         y_probas = bag_model.predict_proba(self.x_test)
 
         #Determining accuracy of the test image
         bag_pred_item=bag_model.predict(item)
         bag_value=bag_pred_item[0]
 
-        return bag_model,bag_pred,y_probas,bag_acc_score,title,self.category[bag_value-1],self.x_trg,self.y_trg,self.y_test
+        return bag_model,bag_pred,y_probas,title,self.categories[bag_value],self.x_trg,self.y_trg,self.y_test
